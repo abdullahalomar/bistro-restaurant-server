ae921577ae9095d995184c87f2959c4835c34b15
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-09-16 02:43:35 +0600

    add to cart api created

--- index.js
+++ index.js
@@ -40,7 +40,18 @@ async function run() {
       res.send(result);
     });
 
-    // cart collection
+    // cart collection api
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post('/carts', async(req, res) => {
       const item = req.body;
       console.log(item);
