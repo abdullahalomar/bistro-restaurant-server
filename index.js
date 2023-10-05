b726453413ae11ffc1d6d299c2ad9a6fd37edeab
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-10-05 19:14:19 +0600

    payment api create

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -157,12 +158,6 @@ async function run() {
       
     })
 
-    // app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req, res) =>{
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await menuCollection.deleteOne(query);
-    //   res.send(result);
-    // })
 
     // review collection api
     app.get('/review', async(req, res) => {
@@ -201,6 +196,24 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+
+    // create payment intent
+    app.post('/create-payment-intent', async(req, res) => {
+        const {price} = req.body;
+        const amount = price * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card'] 
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
