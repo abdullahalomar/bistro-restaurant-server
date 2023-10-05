82ad6f82436ba0c379286e1c104774c1ace3c263
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-10-05 22:46:15 +0600

    get admin astats data from derver

--- index.js
+++ index.js
@@ -226,6 +226,21 @@ async function run() {
     })
 
 
+    app.get('/admin-stats', verifyJWT, verifyAdmin, async(req,res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount()
+
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
+
+      res.send({
+        users,
+        products,
+        orders,
+        revenue
+      })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
